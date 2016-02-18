b786ea9c786f09f509c240e74408438312d4cec6
ch8154 <ch8154@gmail.com>
2016-02-18 19:45:46 +0800

    new moudule for database

---
+++ grandroid-database/src/main/java/com/grandroid/database/Table.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.grandroid.database;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ *
+ * @author Rovers
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Table {
+    /**
+     * 
+     * @return
+     */
+    String value() default "app";//value=""
+}
