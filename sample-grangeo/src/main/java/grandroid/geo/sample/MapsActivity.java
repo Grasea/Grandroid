a0715a7c568f1fdecd74c5f88b58a52e3c230e3b
roversyang@gmail.com <M5Vm5hf8>
2016-03-02 15:01:03 +0800

    modify grangeo to support new Google Service add sample projects

---
+++ sample-grangeo/src/main/java/grandroid/geo/sample/MapsActivity.java
@@ -0,0 +1,45 @@
+package grandroid.geo.sample;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import grandroid.geo.GeoFace;
+import grandroid.view.LayoutMaker;
+
+public class MapsActivity extends GeoFace implements OnMapReadyCallback {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LayoutMaker maker=new LayoutMaker(this);
+        try {
+            insertMap(maker,maker.layFF());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney and move the camera
+        LatLng taipei = new LatLng(24.12, 122.3);
+        googleMap.addMarker(new MarkerOptions().position(taipei).title("Marker in Taipei"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(taipei));
+    }
+}
